1702f90f6267a47d99341092fe0330de1310b9b8
Vankani Arjun <vankaniarjun0103@gmail.com>
2021-07-12 13:11:26 +0530

    "Ecom "

---
+++ app/src/main/java/com/example/ecom/activies/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.ecom.activies;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.ecom.R;
+import com.example.ecom.fragments.HomeFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    Fragment homefragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        homefragment = new HomeFragment();
+        loadFragment(homefragment);
+    }
+
+    private void loadFragment(Fragment homefragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.home_container, homefragment);
+        transaction.commit();
+    }
+}
---
+++ app/src/main/java/com/example/ecom/fragments/HomeFragment.java
@@ -0,0 +1,89 @@
+package com.example.ecom.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.example.ecom.R;
+import com.example.ecom.adeptor.CategoryAdapter;
+import com.example.ecom.adeptor.CategoryModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class HomeFragment extends Fragment {
+
+    RecyclerView catRecycleView;
+    CategoryAdapter categoryAdapter;
+    List<CategoryModel> categoryModelList;
+    FirebaseFirestore db;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        catRecycleView = root.findViewById(R.id.rec_category);
+
+        db   = FirebaseFirestore.getInstance();
+        //image slider
+
+        ImageSlider imageSlider = root.findViewById(R.id.image_slider);
+        List<SlideModel> slideModels = new ArrayList<>();
+
+        slideModels.add(new SlideModel(R.drawable.banner1, "Discount on Shoes Item", ScaleTypes.CENTER_CROP));
+
+        slideModels.add(new SlideModel(R.drawable.banner2, "Discount on Perfume", ScaleTypes.CENTER_CROP));
+
+        slideModels.add(new SlideModel(R.drawable.banner3, "70% OFF", ScaleTypes.CENTER_CROP));
+        imageSlider.setImageList(slideModels);
+
+
+        catRecycleView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+        categoryModelList = new ArrayList<>();
+        categoryAdapter = new CategoryAdapter(getContext(), categoryModelList);
+        catRecycleView.setAdapter(categoryAdapter);
+
+        db.collection("Category")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                CategoryModel categoryModel = document.toObject(CategoryModel.class);
+                                categoryModelList.add(categoryModel);
+                                categoryAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                        }
+                    }
+                });
+
+        return root;
+    }
+}
